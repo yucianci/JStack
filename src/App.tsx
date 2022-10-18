bc3345a35af1e6bb805f59deb976fdae36b10b4d
Yuri Cianci <yucianci@gmail.com>
2022-10-18 03:34:10 -0300

    :tada: First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <header>
+      <h1>
+        My default application
+      </h1>
+    </header>
+  </div>
+);
+
+export default App;
