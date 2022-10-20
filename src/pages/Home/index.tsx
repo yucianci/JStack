b6a200ad12224cfe3014cb0897c81cd1a50de09e
Yuri Cianci <yucianci@gmail.com>
2022-10-20 03:16:54 -0300

    :lipstick: feat: added styles in homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { CiDark, CiBrightnessDown } from 'react-icons/ci';
 
-const Home = () => (
-  <div>
-    <p>
-      Welcome to my app!
-    </p>
-  </div>
-);
+import { Header } from './styled';
+
+const Home = () => {
+  const darkMode = true;
+
+  return (
+    <Header>
+      <h1>JStack</h1>
+      <span>{darkMode ? <CiBrightnessDown /> : <CiDark />}</span>
+    </Header>
+  );
+};
 
 export default Home;
